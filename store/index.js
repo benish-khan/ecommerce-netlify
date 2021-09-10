f5fc8b17fc937b1c5d601fedbec8457b88e89552
Benish Khan <bsarinelli@gmail.com>
2021-09-09 19:13:58 -0700

    Revert update to index.js file

--- store/index.js
+++ store/index.js
@@ -69,7 +69,7 @@ export const actions = {
     try {
       // Create a PaymentIntent with the information about the order
       const result = await axios.post(
-        "https://affectionate-beaver-1774fd.netlify.app/.netlify/functions/create-payment-intent",
+        "https://ecommerce-netlify.netlify.app/.netlify/functions/create-payment-intent",
         {
           items: getters.cartItems
         },
